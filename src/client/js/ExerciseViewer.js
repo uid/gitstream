2d154de2f9d158e910afc66b264208686944f23a
Nick Hynes <nhynes@mit.edu>
2014-09-16 00:04:31 -0400

    Added ExerciseViewer (client ExerciseMachine)

---
+++ src/client/js/ExerciseViewer.js
@@ -0,0 +1,49 @@
+/**
+ * A Mealy machine that represents multi-step exercises as states.
+ * This class is intended to be connected to an ExerciseMachine via an EventEmitter
+ *
+ * @param {Object} config @see ExerciseViewerConfigExample.js for configuration parameters
+ * @param {EventEmitter} eventEmitter the emitter of ExerciseMachine eventEmitter
+ * @param {Object} context data that will be passed as `this` to functions defined in config
+ */
+function ExerciseViewer( config, eventEmitter, context ) {
+    'use strict';
+
+    if ( !(this instanceof ExerciseViewer) ) { return new ExerciseViewer.apply( null, arguments );}
+
+    var stateEntry,
+        states = {},
+        currentState,
+        onHalt = config.onHalt || function() {},
+        onDing = config.onDing || function() {},
+        transitionContext = context || {};
+
+    for ( stateEntry in config ) {
+        if ( config.hasOwnProperty( stateEntry ) ) {
+            states[ stateEntry ] = config[ stateEntry ];
+        }
+    }
+
+    eventEmitter.on( 'step', function( newState, oldState ) {
+        var oldStateDef = states[ oldState ],
+            newStateDef = states[ newState ];
+
+        if ( oldStateDef && typeof oldStateDef[ newState ] === 'function' ) {
+            oldStateDef[ newState ].call( transitionContext );
+        }
+        if ( newStateDef && typeof newStateDef.onEnter === 'function' ) {
+            newStateDef.onEnter.call( transitionContext );
+        }
+        currentState = newState;
+    });
+
+    eventEmitter.on( 'halt', function() {
+        onHalt.call( transitionContext, currentState );
+    });
+
+    eventEmitter.on( 'ding', function() {
+        onDing.call( transitionContext, currentState );
+    });
+}
+
+module.exports = ExerciseViewer;
---
+++ test/client/ExerciseViewerTest.js
@@ -0,0 +1,80 @@
+var ExerciseViewer = require('../../src/client/js/ExerciseViewer'),
+    EventEmitter = require('events').EventEmitter;
+
+module.exports = {
+    setUp: function( done ) {
+        this.events = new EventEmitter();
+        done();
+    },
+
+    testStepInit: function( test ) {
+        test.expect( 2 );
+
+        var ev = new ExerciseViewer({
+            testState: {
+                onEnter: function() {
+                    test.ok( true );
+                    test.strictEqual( this.test, 'testing' );
+                    test.done();
+                }
+            }
+        }, this.events, { test: 'testing' } );
+
+        this.events.emit( 'step', 'testState', null );
+    },
+
+    testStepStep: function( test ) {
+        test.expect( 3 );
+
+        var ev = new ExerciseViewer({
+            testState: {
+                testState2: function() {
+                    test.ok( true );
+                    this.testState = 'wuz here';
+                }
+            },
+            testState2: {
+                onEnter: function() {
+                    test.ok( true );
+                    test.strictEqual( this.testState, 'wuz here' );
+                    test.done();
+                }
+            }
+        }, this.events );
+
+        this.events.emit( 'step', 'testState', null );
+        this.events.emit( 'step', 'testState2', 'testState' );
+    },
+
+    testStepHalt: function( test ) {
+        test.expect( 2 );
+
+        var ev = new ExerciseViewer({
+            onHalt: function( haltState ) {
+                test.strictEqual( this.hey, 'did you halt?' );
+                test.strictEqual( haltState, 'haltsHere' );
+                test.done();
+            },
+            haltsHere: {}
+        }, this.events, { hey: 'did you halt?' } );
+
+        this.events.emit( 'step', 'haltsHere', null );
+        this.events.emit( 'halt' );
+    },
+
+    testStepDing: function( test ) {
+        test.expect( 2 );
+
+        var ev = new ExerciseViewer({
+            onDing: function( dingState ) {
+                test.strictEqual( this.hey, 'this is not an RPG' );
+                test.strictEqual( dingState, 'levelUp' );
+                test.done();
+            },
+            levelUp: {}
+        }, this.events, { hey: 'this is not an RPG' } );
+
+        this.events.emit( 'step', 'levelUp', null );
+        this.events.emit( 'ding' );
+    }
+};
