5e139563626ca605824e278f1aed69a1241bd93f
Nick Hynes <nhynes@mit.edu>
2014-09-16 23:51:22 -0400

    Added utilities for assessing state of repos

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,15 +1,30 @@
-var shoe = require('shoe'),
-    stream = shoe('http://localhost:4242/events'),
-    emitter = require('duplex-emitter'),
-    events = emitter( stream ),
-    logger = function( prefix ) {
-        return function() {
-            console.log( prefix, arguments );
-        }
-    };
+'use strict';
 
-events.on('sync', logger('sync') );
-events.on( 'step', logger('step') );
-events.on( 'ding', logger('ding') );
-events.on( 'halt', logger('halt') );
-// events.emit('exerciseChanged', 'exercise1');
+var shoe = require('shoe'),
+    _ = require('lodash'),
+    stream = shoe('http://localhost:4242/events'),
+    streamEmitter = require('duplex-emitter'),
+    eventEmitter = require('event-emitter'),
+    events = streamEmitter( stream ),
+    exerciseEvents = eventEmitter({}),
+    triggerExerciseEvent = function( eventName ) {
+        return function() {
+            exerciseEvents.emit.apply( null, [ eventName ].concat( arguments ) );
+        }
+    },
+    state = {};
+
+events.on('sync', function( newState ) {
+    state = _.defaults( state, newState );
+    console.log( state );
+    // if key is received, store it
+    // load exercise config
+    // create new exercise machine
+
+    events.emit('exerciseChanged', 'exercise1');
+});
+
+// forward exercise events to exercise machine emitter
+events.on( 'step', triggerExerciseEvent('step') );
+events.on( 'halt', triggerExerciseEvent('halt') );
+events.on( 'ding', triggerExerciseEvent('ding') );
