6b250c692f66dcb745941560ff1a595e1b3f39e5
Nick Hynes <nhynes@mit.edu>
2015-01-31 11:07:49 -0500

    Updated vagrant init and added README for development

--- README.md
+++ README.md
@@ -24,3 +24,25 @@ $ node node_modules/forever/bin/forever start dist/server/main.js
 ```
 
 Navigate your browser to your server and GitStream away!
+
+## Development
+
+For active development, you will want to have node and npm installed on your system.  
+If you only want to work inside of the VM, omit the initial `make`.
+
+```
+$ make
+$ cd vagrant
+$ vagrant up && vagrant ssh
+```
+
+Once in the VM, start the server by running
+
+```
+$ sudo su gitstream
+$ node /opt/gitstream/dist/server/main
+```
+
+You can now view GitStream by navigating your browser to [http://localhost:8080](http://localhost:8080).
+
+To re-build GitStream when changes are made, `npm install -g gulp` and run `gulp` from somewhere within the project directory. You will have to manually restart the server after editing anything in src/server.
--- vagrant/init.sh
+++ vagrant/init.sh
@@ -1,25 +1,18 @@
 # install required packages
 apt-get update
-apt-get -y install git mongodb nginx nodejs npm redis-server
+apt-get -y install git mongodb-server nginx nodejs redis-server nodejs-legacy
 
-# link nodejs to node
-ln -s /usr/bin/nodejs /usr/bin/node
-
-# make the repos directory
-mkdir /srv/repos
-
-# make the mongodb db directory
-mkdir /var/opt/gitstream/mongo
+# make the repos and database directory
+mkdir -p /srv/repos /var/opt/gitstream/mongo
 
 # create gitstream user and give it perms
 useradd -m gitstream
-chown -R gitstream:gitstream /srv/repos
-chown -R gitstream:gitstream /var/log/nginx
+chown -R gitstream:gitstream /srv/repos /var/log/nginx /var/opt/gitstream
 
 # add the vagrant user to gitstream for convenience
 usermod -G gitstream vagrant
 
-# move the nginx config file into place and restart the server
+# move the nginx config file into place and stop the server
 ln -fs /opt/gitstream/nginx.conf /etc/nginx/nginx.conf
 killall nginx
 
@@ -29,7 +22,7 @@ su gitstream -c 'git config --global user.name "GitStream"'
 
 # build gitstream if it hasn't already been built
 if [ ! -d "/opt/gitstream/dist" ]; then
+    apt-get install npm
     cd /opt/gitstream
-    npm install --production
-    NODE_ENV=production node ./node_modules/gulp/bin/gulp build
+    make build
 fi
