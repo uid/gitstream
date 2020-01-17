f19a4937889d003aba022caed78b7fc5ac102eff
Rob Miller <rcm@mit.edu>
2020-01-16 20:50:08 -0500

    tests now pass again

--- src/client/js/ExerciseViewer.js
+++ src/client/js/ExerciseViewer.js
@@ -41,7 +41,7 @@ function ExerciseViewer( config, eventEmitter ) {
                 newStateDef.onEnter.call( config, output, doneCb )
             }
         } else {
-            doneCb(oldStateDef[ newState ])
+            doneCb(oldStateDef && oldStateDef[ newState ])
         }
         this._currentState = newState
     }.bind( this ) )
