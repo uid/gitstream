6a06e194bc86a51fb28dce4d411be309d30fd16d
Javier Solis <javsolis@mit.edu>
2023-10-26 13:30:59 -0400

    added path variables used in makefile

--- paths.mk
+++ paths.mk
@@ -5,3 +5,13 @@ GSDIRS = $(DEST) $(DESTDIR)/srv/repos $(DESTDIR)/var/opt/gitstream/mongo
 NGINXLOGS = $(DESTDIR)/var/log/nginx
 GSLOGS = $(NGINXLOGS)/gitstream_access.log $(NGINXLOGS)/gitstream_error.log
 GITSTREAM_USER := $(shell grep gitstream /etc/passwd; echo $$?)
+
+# source paths
+SRC_CLIENT_STATIC = src/client/resources/ src/client/*.html
+SRC_SERVER = src/server/
+SRC_EXERCISES = node_modules/gitstream-exercises/exercises
+
+# destination paths
+DIST_CLIENT = dist/client/
+DIST_SERVER = dist/server/
+DIST_EXERCISES = dist/server
