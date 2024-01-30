51e1c2c0ad6427ebda498edfc0188a1e480f7e8d
Javier Solis <javsolis@mit.edu>
2024-01-29 23:05:48 -0500

    update node version to 20

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install git nginx make
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ca-certificates gnupg || exit 1
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=18
+NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install nodejs || exit 1
