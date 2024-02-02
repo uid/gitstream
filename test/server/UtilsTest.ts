4076c6ba7ccf85a91c1c58639a5180a7bc30f6fe
Javier Solis <javsolis@mit.edu>
2024-02-01 19:42:56 -0500

    Ported UtilsTest.mjs to TypeScript

---
+++ test/server/UtilsTest.ts
@@ -0,0 +1,21 @@
+import { utils } from '../../src/server/utils'
+import assert from 'assert'
+
+describe('Utils', () => {
+  it('testEventsToPropsDefaultPrefix', function (){
+    const eventProps = utils.events2Props( [ 'test', 'a-test', 'a-nother-test', 'aTesT' ] )
+    assert.strictEqual( eventProps.onTest, 'test' )
+    assert.strictEqual( eventProps.onATest, 'a-test' )
+    assert.strictEqual( eventProps.onANotherTest, 'a-nother-test' )
+    assert.strictEqual( eventProps.onATesT, 'aTesT' )
+
+  })
+
+  it('testEventsToPropsCustomPrefix', function (){
+    const eventProps = utils .events2Props( [ 'handle', 'on' ], [ 'test', 'a-test' ] )
+    assert.strictEqual( eventProps.onTest, 'test' )
+    assert.strictEqual( eventProps.handleTest, 'test' )
+    assert.strictEqual( eventProps.onATest, 'a-test' )
+    assert.strictEqual( eventProps.handleATest, 'a-test' )
+  })
+})
