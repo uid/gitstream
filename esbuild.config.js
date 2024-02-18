41fcd7b2e03f742faf339b5dd1dcbaf00257136d
Javier Solis <javsolis@mit.edu>
2024-02-18 17:08:02 -0500

    Adjust appropriate filepaths from .js to .cjs

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,7 +2,7 @@ const esbuild = require('esbuild');
 const handlebarsPlugin = require('esbuild-plugin-handlebars');
 
 esbuild.build({
-  entryPoints: ['src/client/js/main.js'],
+  entryPoints: ['src/client/js/main.cjs'],
   plugins: [handlebarsPlugin()],
   outdir: 'dist/client',
   bundle: true,
