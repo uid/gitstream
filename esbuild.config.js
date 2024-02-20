5718d52dcb4ec575c14f5ed18cb3e5db4b62f4f7
Javier Solis <javsolis@mit.edu>
2024-02-20 16:18:41 -0500

    modify esbuild.config.js (but still does not produce desired outcome)

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,11 +1,14 @@
-const esbuild = require('esbuild');
-const handlebarsPlugin = require('esbuild-plugin-handlebars');
+import esbuild from "esbuild";
+import handlebarsPlugin from "esbuild-plugin-handlebars";
 
-esbuild.build({
-  entryPoints: ['src/client/js/main.cjs'],
-  plugins: [handlebarsPlugin()],
-  outdir: 'dist/client',
-  bundle: true,
-  platform: 'node',
-  format: 'cjs'
-})
+esbuild
+  .build({
+    entryPoints: ["./src/client/js/main.cjs"],
+    outfile: "dist/src/client/main.js",
+    bundle: true,
+    plugins: [handlebarsPlugin()],
+    // minify: true,
+    format: 'cjs'
+  })
+  .then((result) => console.log(result))
+  .catch(() => process.exit(1));
