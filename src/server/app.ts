1c6415d6a4bfe2de83442458d4398d11f589c53b
Javier Solis <javsolis@mit.edu>
2024-03-15 18:47:36 -0400

    move all app-related statements into configureApp

--- src/server/app.ts
+++ src/server/app.ts
@@ -12,16 +12,6 @@ import * as routesUtils from './routesUtils.js';
 export const app = express();
 export const PORT = 4242; // for WebSocket connection
 
-// set up a session cookie to hold the user's identity after authentication
-const sessionParser = session({
-    secret: settings.sessionSecret || crypto.pseudoRandomBytes(16).toString('hex'),
-    sameSite: 'lax',
-    signed: true,
-    overwrite: true,
-});
-
-app.use(sessionParser);
-
 // Extend the Express Request to include the user property and the session from cookie-session
 interface AuthenticatedRequest extends Request {
     user: {
@@ -37,28 +27,37 @@ interface AuthenticatedRequest extends Request {
     }
 }
 
-// this middleware sets req.user to an object { username:string, fullname:string }, either from
-// session cookie information or by authenticating the user using the authentication method selected in settings.js.
-//
-// By default there is no authentication method, so this method authenticates as a guest user with a randomly-generated username.
-let setUser = function(req: AuthenticatedRequest, res: Response, next: NextFunction) {
-    if (!req.user) {
-        if (req.session.guest_user){
-            req.user = req.session.guest_user;
-        } else {
-            req.user = req.session.guest_user = {
-                username: routesUtils.createRandomId(),
-                fullname: "Guest User"
-            };
-        }
-    }
-    console.log('guest user connected as', req.user);
-    next();
-};
-
-let setUserAuthenticateIfNecessary = setUser; 
-
 async function configureApp() {
+    // set up a session cookie to hold the user's identity after authentication
+    const sessionParser = session({
+        secret: settings.sessionSecret || crypto.pseudoRandomBytes(16).toString('hex'),
+        sameSite: 'lax',
+        signed: true,
+        overwrite: true,
+    });
+
+    app.use(sessionParser);
+
+    // this middleware sets req.user to an object { username:string, fullname:string }, either from
+    // session cookie information or by authenticating the user using the authentication method selected in settings.js.
+    // By default there is no authentication method, so this method authenticates as a guest user with a randomly-generated username.
+    let setUser = function(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+        if (!req.user) {
+            if (req.session.guest_user){
+                req.user = req.session.guest_user;
+            } else {
+                req.user = req.session.guest_user = {
+                    username: routesUtils.createRandomId(),
+                    fullname: "Guest User"
+                };
+            }
+        }
+        console.log('guest user connected as', req.user);
+        next();
+    };
+
+    let setUserAuthenticateIfNecessary = setUser; 
+
     // if we have settings for OpenID authentication, configure it
     // this should always be the case, unless you're debugging and want to generate random
     // usernames per session
